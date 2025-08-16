195edb8fd792e998e7260bb8600f3d45eec3391c
Joey Barten - Founder Orbyss <joey.barten@unfussiness.io>
2025-08-16 22:04:20 +0200

    Update TranslationSection.cs

--- src/TranslationSection.cs
+++ src/TranslationSection.cs
@@ -1,5 +1,8 @@
-﻿namespace Orbyss.Components.Json.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Orbyss.Components.Json.Models
 {
+    [JsonConverter(typeof(TranslationSectionJsonConverter))]
     public sealed record TranslationSection(
         string? Label,
         TranslationErrorSection? Error,
